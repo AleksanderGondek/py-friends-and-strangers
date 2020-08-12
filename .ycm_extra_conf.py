eda80c13653b6208c8d178feb6283f5279abc6bf
Aleksander Gondek <gondekaleksander@protonmail.com>
2020-08-12 23:55:39 +0200

    Added python path for vim-ycm

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -5,5 +5,10 @@ def Settings(**kwargs):
   return {
     "interpreter_path": pathlib.Path(__file__)
       .parent.absolute()
-      .joinpath(".venv/bin/python")
+      .joinpath(".venv/bin/python"),
+    "sys_path": [
+      pathlib.Path(__file__)
+        .parent.absolute()
+        .joinpath("py_friends_and_strangers")
+    ]
   }
